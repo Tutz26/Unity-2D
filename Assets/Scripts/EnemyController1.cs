12dfce85033142943b07ce0fa3d1a50b2de7a9b3
Tutz26 <tutz_26@hotmail.com>
2020-03-04 09:42:53 -0700

    Created enemy basic logic

---
+++ Assets/Scripts/EnemyController1.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController1 : MonoBehaviour
+{
+
+
+        public float fireDelay = 1f;
+        private float maxPositionX = 1;
+        private float minPositionX = 0;
+        private bool movingRight = true;
+        public float movementSpeed = 1f;
+
+
+    // Start is called before the first frame update
+        void Start()
+    {
+
+        //Tamaño pantalla
+        //Screen.Width
+        //Tamaño de pantalla
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        maxPositionX = 1;
+        minPositionX = 0;
+
+        //Get viewport sizes to x and y
+        var pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp01(pos.x);
+        pos.y = Mathf.Clamp01(pos.y);
+
+        // transform.position = Camera.main.ViewportToWorldPoint(pos);
+
+
+        if(movingRight){
+            //Move right
+            transform.Translate(new Vector2(movementSpeed, 0f));
+
+            //Revisar si llga al limite en x permitido
+            if(pos.x >= maxPositionX){
+                movingRight = false;
+            }
+        } else {
+            //Move Left
+            transform.Translate(new Vector2(-movementSpeed, 0f));
+
+            //Revisar si llga al limite en x permitido
+            if(pos.x <= minPositionX){
+                movingRight = true;
+            }
+
+        }
+        
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject projectile;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalMovement = Input.GetAxis("Vertical");
+        float horizontalMovement = Input.GetAxis("Horizontal");
+
+        if(verticalMovement != 0f || horizontalMovement != 0f){
+            Vector2 newPosition = new Vector2(horizontalMovement * 0.1f, (verticalMovement * 0.1f));
+            transform.Translate(newPosition);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Debug.Log("Pew");
+            Instantiate(projectile, transform.position, transform.rotation);
+        }
+
+    }
+
+}
